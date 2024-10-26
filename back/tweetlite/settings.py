a1f3c0e7fe3698bf601101ce5885b30a71246cd7
ivanvarella <ivanvarella@gmail.com>
2024-10-26 12:18:12 -0300

    Minor changes on settings

--- back/tweetlite/settings.py
+++ back/tweetlite/settings.py
@@ -130,20 +130,19 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
-# TODO: Descomentar as validações
 AUTH_PASSWORD_VALIDATORS = [
-    # {
-    #     "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
-    # },
-    # {
-    #     "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
-    # },
-    # {
-    #     "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
-    # },
-    # {
-    #     "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
-    # },
+    {
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+    },
 ]
 
 
@@ -194,7 +193,6 @@ REST_FRAMEWORK = {
 }
 
 # Configurações do Simple JWT
-# TODO: Alterar os tempos de expiração dos Tokens
 SIMPLE_JWT = {
     "ROTATE_REFRESH_TOKENS": env("ROTATE_REFRESH_TOKENS"),
     "BLACKLIST_AFTER_ROTATION": env("BLACKLIST_AFTER_ROTATION"),
